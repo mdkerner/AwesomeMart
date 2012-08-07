5511b26c71ae6e078b2e3706a7330c53df7faef5
Michael Kerner <mdkerner@gmail.com>
2012-08-07 16:20:16 -0700

    Initial Commit

---
+++ AwesomeMart/AwesomeMart.Data/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AwesomeMart.Data
+{
+    abstract protected class Person
+    {
+        public virtual string Name { get; set; }
+        public virtual int ID { get; set; }
+    }
+}
