8546c0908311b419ca3cd391c4223769309e88b8
Michael Kerner <mdkerner@gmail.com>
2012-08-08 13:33:14 -0700

    Added some classes/enums

--- AwesomeMart/AwesomeMart.Model/Employee.cs
+++ AwesomeMart/AwesomeMart.Model/Employee.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using AwesomeMart.Data;
 
 namespace AwesomeMart.Model
 {
     public class Employee : Person
     {
-        public Employee()
-        {
-            
-        }
+        public DateTime StartDate { get; set; }
+        public Position Position { get; set; }
+        public float PayRate { get; set; }
+        public PayType PayType { get; set; }
     }
 }
