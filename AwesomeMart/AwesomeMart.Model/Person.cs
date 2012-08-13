a5035dd7fb68a2367c320107cb8bd88e6f53fe21
Lynne <lynne.kirsch@gmail.com>
2012-08-13 14:26:25 -0400

    Added new properties to persons.

--- AwesomeMart/AwesomeMart.Model/Person.cs
+++ AwesomeMart/AwesomeMart.Model/Person.cs
@@ -8,6 +8,12 @@ namespace AwesomeMart.Model
     abstract public class Person
     {
         public virtual string Name { get; set; }
+        public virtual string Race { get; set; }
+        public virtual string Address { get; set; }
+        public virtual int CreateDate { get; set; }
         public virtual int ID { get; set; }
+        public virtual int Age { get; set; }
+        public virtual int Phone { get; set; }
+        
     }
 }
